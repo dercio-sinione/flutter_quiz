e6db078ad5ad49b94d545efdae14ca9515971389
dercio-sinione <derciosinione@gmail.com>
2021-07-17 21:30:21 +0100

    Dart Pratice class

--- lib/dartpratice.dart
+++ lib/dartpratice.dart
@@ -1,3 +1,15 @@
+void main() {
+  var person = Person();
+  print(person.name);
+
+  double firstResult = addNumbers(2, 4.5);
+  double secondResult = addNumbers(15, 10.5);
+
+  print('First result: $firstResult');
+  print('Second result: $secondResult');
+  print('Total: ${firstResult + secondResult}');
+}
+
 class Person {
   String name = 'Dercio Derone';
   int age = 21;
@@ -5,15 +17,4 @@ class Person {
 
 addNumbers(num num1, num num2) {
   return num1 + num2;
-}
-
-void main() {
-  String name = 'Dercio';
-  int age = 21;
-  double firstResult = addNumbers(2, 4.5);
-  double secondResult = addNumbers(15, 10.5);
-
-  print('First result: $firstResult');
-  print('Second result: $secondResult');
-  print('Total: ${firstResult + secondResult}');
 }
