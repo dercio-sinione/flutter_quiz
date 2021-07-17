e222853faff7805af2e79ae32ef2c868b17e83b9
dercio-sinione <derciosinione@gmail.com>
2021-07-17 21:38:47 +0100

    Dart Pratice / Functions

--- lib/dartpratice.dart
+++ lib/dartpratice.dart
@@ -4,12 +4,9 @@ void main() {
   person.age = 21;
   print('Hello $person.name, your age is $person.age');
 
-  double firstResult = addNumbers(2, 4.5);
-  double secondResult = addNumbers(15, 10.5);
+  if (person.age < 18) return;
 
-  print('First result: $firstResult');
-  print('Second result: $secondResult');
-  print('Total: ${firstResult + secondResult}');
+
 }
 
 class Person {
@@ -19,4 +16,13 @@ class Person {
 
 addNumbers(num num1, num num2) {
   return num1 + num2;
+}
+
+void continueProgram(){
+  double firstResult = addNumbers(2, 4.5);
+  double secondResult = addNumbers(15, 10.5);
+
+  print('First result: $firstResult');
+  print('Second result: $secondResult');
+  print('Total: ${firstResult + secondResult}');
 }
